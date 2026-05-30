4d277c0dd5136da2ebb56441699e00d5b2f7b74f
Swapnil <swapniltyagi.iitk@gmail.com>
2026-05-31 00:36:50 +0530

    adc continuous conversion

---
+++ 7_adc_continuous_conversion/src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "stm32f411re.h"
+#include "usart.h"
+#include "adc.h"
+
+#define GPIOAEN (1U<<0)
+#define PA5     (1U<<5)
+
+uint32_t sensor_value;
+
+int main(void){
+
+    uart2_txrx_init();
+    pa1_adc_init();
+    start_adc_conversion();
+
+    while(1){
+        sensor_value = read_adc_value();
+        printf("Sensor Value: %lu\r\n", sensor_value);
+    }
+    return 0;
+}
+
